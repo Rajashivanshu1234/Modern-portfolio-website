eb81df7af143211a99722885c4eb7aace5563249
shivanshu sharma <shivanshusharma677@gmail.com>
2026-06-23 16:08:14 +0530

    Update files

--- app.js
+++ app.js
@@ -18,5 +18,22 @@ videoList.forEach (function(video){
     hoverSign.classList.remove("active")
 })
 })
+const contactform = document.getElementById('contact');
+
+emailjs.init('hzUlsSsnSBHFI1jQL'); // Public key from EmailJS
+
+const sendEmail = async (e) => {
+e.preventDefault();
+try {
+await emailjs.sendForm('service_ak5upvg', 'template_jkovdkp', '#contact-form');
+alert('Message sent successfully!');
+contactform.reset();
+} catch (err) {
+console.error(err);
+alert('Message not sent. Check console for details.');
+}
+};
+
+contactform.addEventListener('submit', sendEmail);
 
 
